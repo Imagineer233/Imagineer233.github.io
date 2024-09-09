afb5fa0abf3b3219326dd460469c9992563ae4bb
Imagineer233 <147693808+Imagineer233@users.noreply.github.com>
2024-09-09 11:14:32 +0000

    提交文章测试 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b156.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
+var posts=["posts/c31b427f.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
